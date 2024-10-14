8b7bbec26363099d04fbbd34fba70d02daea48cb
okano <okano.t@world-wing.com>
2024-10-15 00:35:25 +0900

    fix page style

--- src/components/TechStackGenerateContainer.tsx
+++ src/components/TechStackGenerateContainer.tsx
@@ -69,9 +69,6 @@ export default function TechStackGenerateContainer() {
 
   return (
     <div className="max-w-2xl mx-auto p-6 space-y-8">
-      <h1 className="text-5xl font-bold text-center">
-        Github README Tech Stack Generator
-      </h1>
       <div className="space-y-4">
         <Input
           id="techs"
